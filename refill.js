0e56a3f1484338873f24535a0a5059891fe448c7
Ken Graf <kengraf57@gmail.com>
2024-02-12 10:30:48 -0500

    Create refill.js

---
+++ refill.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    console.log('Received SNS event:', JSON.stringify(event, null, 2));
+    
+    // Define parameters for DynamoDB operation
+    const params = {
+        TableName: 'Eeny-redo',
+        Item: { name: "Game Over" }
+    };
+
+    try {
+        // Put item in DynamoDB table
+        await dynamoDB.put(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Record added successfully')
+        };
+    };
+  } catch (err) {
+    statusCode = 400;
+    body = "Lambda error: " + JSON.stringify(err.message);
+  }
+  return {
+    statusCode,
+    body,
+  };
+};
