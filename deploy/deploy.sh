84a6fd396624866e99b95ce154727a5b7ad75dfc
Ken Graf <kengraf57@gmail.com>
2024-02-15 08:36:50 -0500

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,25 +14,23 @@ S3BUCKET=$STACK_NAME-$(tr -dc a-f0-9 </dev/urandom | head -c 10)
 sed -ri "s/undefined-bucket/${S3BUCKET}/" parameters.json
 sed -ri "s/${STACK_NAME}-[0-9a-f]*/${S3BUCKET}/" parameters.json
 
-echo "Create S3 bucket..."
+echo "Create and load S3 bucket..."
 aws s3api create-bucket --bucket  ${S3BUCKET} --region ${AWS_REGION} --create-bucket-configuration LocationConstraint=${AWS_REGION}
-
-echo "Creating stack..."
-
 # upload lambda functions
 cd lambda/fetch
 zip fetch.zip -xi index.js
-aws s3 cp fetch.zip s3://${S3BUCKET}/deploy/lambda/fetch.zip
+aws s3 cp fetch.zip s3://${S3BUCKET}/fetch.zip
 cd ../..
 cd lambda/reload
 zip reload.zip -xi index.js
-aws s3 cp reload.zip s3://${S3BUCKET}/deploy/lambda/reload.zip
+aws s3 cp reload.zip s3://${S3BUCKET}/reload.zip
 cd ../..
 
+echo "Creating stack..."
 # upload cf stack
 aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://cfStack.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags file://tags.json --output text
 STACK_ID=`aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].StackId" --output text`
 
 echo "Waiting on ${STACK_ID} create completion..."
 aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
-aws cloudformation describe-stacks --stack-name ${STACK_ID} --query Outputs
+aws cloudformation describe-stacks --stack-name ${STACK_ID} --query Stacks[0].StackName --output text
